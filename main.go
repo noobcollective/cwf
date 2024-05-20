4200288360d77ec8e18220d9c59a15012df22002
iCulture <99724871+iCultureBud@users.noreply.github.com>
2024-05-20 16:40:03 +0200

    [GENERAL] change -r flag to boolean

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ var (
 var (
 	list     = flag.Bool("l", false, "List files.")
 	deletion = flag.Bool("d", false, "Delete file.")
-	register = flag.String("r", "", "Register a user on server for the first time.")
+	register = flag.Bool("r", false, "Register a user on server for the first time.")
 )
 
 func main() {
